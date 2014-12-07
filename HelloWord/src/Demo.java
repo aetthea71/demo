ed21eab3dddb34195f56dcd076713ba1ec9ea5a0
aetthea71 <aetthea@gmail.com>
2014-12-07 17:37:35 +0700

    inital checking of sales

---
+++ HelloWord/src/Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int xx, yy;
+		xx=10; yy=20;
+        System.out.println("Jumlah xx + yy : "+ sum(xx,yy));
+	}
+	
+	public static int sum(int x, int y){		
+		return (x+y);
+	}
+
+}
